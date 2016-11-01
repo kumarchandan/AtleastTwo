4e978f028a8c9c003797a57111328f655250c727
K.Chan <chandankumar99341@gmail.com>
2016-11-01 15:15:37 +0530

    longest common subsequence

---
+++ src/com/array/CheckIfArrayElementsAreConsecutive.java
@@ -0,0 +1,13 @@
+package com.array;
+
+// http://www.geeksforgeeks.org/check-if-array-elements-are-consecutive/
+// Given an array of elements check if elements in the array are consecutive or not.
+
+
+public class CheckIfArrayElementsAreConsecutive {
+	//
+	public boolean check() {
+		
+		return true;
+	}
+}
---
+++ src/com/dynamicpro/LongestCommonSubsequence.java
@@ -0,0 +1,43 @@
+package com.dynamicpro;
+
+import java.util.ArrayList;
+
+// http://www.geeksforgeeks.org/dynamic-programming-set-4-longest-common-subsequence/
+
+public class LongestCommonSubsequence {
+	
+	//
+	public int lcsDynamic(char str1[], char str2[]) {
+		//
+		int max = 0;
+		int[][] temp = new int[str1.length + 1][str2.length + 1];
+		
+		for (int i = 1; i < temp.length; i++) {
+			for (int j = 1; j < temp[i].length; j++) {
+				// If elements equal
+				if(str1[i - 1] == str2[j - 1]) {
+					temp[i][j] = temp[i - 1][j - 1] + 1;
+				} else {
+					temp[i][j] = Math.max(temp[i-1][j], temp[i][j-1]);
+				}
+				// Record max till now
+				if(temp[i][j] > max) {
+					max = temp[i][j];
+				}
+			}
+		}
+		
+		return max;
+	}
+	
+	//
+	public static void main(String[] args) {
+		LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+		String str2 = "abcdaf";
+        String str1 = "acbcf";
+        
+        int result = lcs.lcsDynamic(str1.toCharArray(), str2.toCharArray());
+        System.out.println(' ');
+        System.out.println(result);
+	}
+}
